6be26f8dd161e31dc713162faa3162bf2155cf3d
ervitis <vitomarti@gmail.com>
2021-03-21 23:56:54 +0100

    chore: let pattern matching

--- ch06/patmatch/src/main.rs
+++ ch06/patmatch/src/main.rs
@@ -1,14 +1,30 @@
+
 fn main() {
     println!("{}", value_in_cents(Coin::Penny));
     println!("{:?}", plus_one(None));
     println!("{}", plus_one(Some(7)).unwrap());
+
+    let some_value = Some(0u8);
+    match some_value {
+        Some(3) => println!("three"),
+        _ => (),
+    };
+
+    let coin = Coin::Quarter(String::from("Alabama"));
+
+    let mut count = 0;
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}", state);
+    } else {
+        count += 1;
+    }
 }
 
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(String),
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -16,7 +32,10 @@ fn value_in_cents(coin: Coin) -> u8 {
         Coin::Penny => 1,
         Coin::Dime => 10,
         Coin::Nickel => 5,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("{} from state", state);
+            1
+        },
     }
 }
 
