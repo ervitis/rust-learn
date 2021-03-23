4ace8a5a29a3fb1f7d8d25bbc8af2aac1ab60964
ervitis <vitomarti@gmail.com>
2021-03-23 14:48:12 +0100

    chore: manipulate vectors

--- ch08/collections/src/main.rs
+++ ch08/collections/src/main.rs
@@ -5,7 +5,16 @@ fn main() {
     v.push(5);
     v.push(6);
 
-    println!("{:?}", v)
+    println!("{:?}", v);
 
     let another_v = vec![1, 2, 3];
+
+    println!("{:?}", another_v);
+
+    fn hello_there(vec: &mut Vec<i32>) {
+        vec.pop();
+    }
+
+    hello_there(&mut v);
+    println!("{:?}", v)
 }
