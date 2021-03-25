43af06b291ce5bc8892ca1eb642d09513bf4123e
ervitis <vitomarti@gmail.com>
2021-03-25 10:29:18 +0100

    chore: hashmap

--- ch08/collections/src/main.rs
+++ ch08/collections/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     let mut v: Vec<i32> = Vec::new();
 
@@ -25,4 +27,16 @@ fn main() {
     for value in another_v {
         println!("{}", value);
     }
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let another_scores: HashMap<_, _> = vec![String::from("Blue"), String::from("Yellow")]
+        .into_iter()
+        .zip(vec![10, 50].into_iter())
+        .collect();
+
+    println!("{:?} and {:?}", scores, another_scores);
+    println!("Blue score is {:?}", scores.get(&String::from("Blue")))
 }
